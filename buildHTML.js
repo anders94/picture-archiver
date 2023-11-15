7630f30144a9406e609cda257be178f27ba8213c
Anders Brownworth <anders-git@evantide.com>
2023-11-15 09:01:48 -0500

    fix index creation

--- buildHTML.js
+++ buildHTML.js
@@ -37,8 +37,11 @@ const buildContent = async (dirPath) => {
 	const template = fs.readFileSync(templateHTML);
 	let html = '';
 	for (const entry of content) {
-	    const thumb = entry[1].split('.')[0] + '-thumbnail.jpg';
-	    html += `<a href='${entry[1]}'><img src='${thumb}' width=128 height=128 class='rounded'></a>\n`;
+	    if (entry[1].indexOf('-thumbnail.jpg') > -1) {
+		const thumb = entry[1].split('.')[0] + '-thumbnail.jpg';
+		html += `<a href='${entry[1]}'><img src='${thumb}' width=128 height=128 class='rounded'></a>\n`;
+
+	    }
 
 	}
 	fs.writeFileSync(path.join(dirPath, 'index.html'), template.toString().replace('@@content', html));
@@ -49,54 +52,61 @@ const buildContent = async (dirPath) => {
 
 };
 
-const recurseForIndex = async (dirPath) => {
-    const files = await fs.readdirSync(dirPath, {withFileTypes: true});
-
-    let foundHTML = false;
-
-    for (const file of files) {
-	if (file.isDirectory() && await recurseForIndex(path.join(dirPath, file.name)))
-	    buildIndex(dirPath);
-	else
-	    if (file.name == 'index.html')
-		foundHTML = true;
-
-    }
-
-    return foundHTML;
-
-};
-
 const buildIndex = async (dirPath) => {
-    console.log(' --', dirPath);
-    const files = await fs.readdirSync(dirPath, {withFileTypes: true});
+    let indexes = {};
+    const years = await fs.readdirSync(dirPath, {withFileTypes: true});
+    for (const year of years) {
+	if (year.isDirectory()) {
+	    const months = await fs.readdirSync(path.join(year.path, year.name), {withFileTypes: true});
+            for (const month of months) {
+		if (month.isDirectory()) {
+		    const days = await fs.readdirSync(path.join(month.path, month.name), {withFileTypes: true});
+		    for (const day of days) {
+			if (day.isDirectory()) {
+			    if (fs.existsSync(path.join(day.path, day.name), 'index.html')) {
+				if (!indexes[year.name])
+				    indexes[year.name] = {};
+				if (!indexes[year.name][month.name])
+				    indexes[year.name][month.name] = {}
+				indexes[year.name][month.name][day.name] = day.path;
 
-    const indexes = [];
+			    }
 
-    for (const file of files)
-	if (file.isDirectory()) {
-	    const subFiles = await fs.readdirSync(path.join(dirPath, file.name), {withFileTypes: true});
-	    for (const subFile of subFiles)
-		if (!subFile.isDirectory() && subFile.name == 'index.html')
-		    indexes.push([file.path, file.name]);
+			}
+
+		    }
+
+		}
+
+	    }
 
 	}
-
-    if (indexes.length > 0) {
-	const template = fs.readFileSync(templateHTML);
-	let html = '<ul  class="list-group">\n';
-	for (const entry of indexes) {
-	    const sanitizedPath = path.join(entry[0].replace(archiveDirPath, ''), entry[1]);
-	    html += `<li class="list-group-item"><a href='${sanitizedPath}'>${sanitizedPath}</a></li>\n`;
-
-	}
-
-	html += '</ul>\n';
-	fs.writeFileSync(path.join(dirPath, 'index.html'), template.toString().replace('@@content', html));
 
     }
-    else
-	console.log('didnt make an index for', dirPath, 'because i didnt see indexes');
+
+    const template = fs.readFileSync(templateHTML);
+
+    let html = '<ul class="list-group">\n';
+    for (const year of Object.keys(indexes))
+	html += `<li class="list-group-item"><a href='./${year}/'>${year}</a></li>\n`;
+    html += '</ul>\n';
+    fs.writeFileSync(path.join(archiveDirPath, 'index.html'), template.toString().replace('@@content', html));
+
+    for (const year of Object.keys(indexes)) {
+	html = '';
+	for (const month of Object.keys(indexes[year])) {
+	    for (const day of Object.keys(indexes[year][month])) {
+		html += `<li class="list-group-item"><a href='./${month}/${day}/'>${year} - ${month} - ${day}</a></li>\n`;
+
+		console.log(year, month, day);
+
+	    }
+
+	}
+	html += '</ul>\n';
+	fs.writeFileSync(path.join(archiveDirPath, year, 'index.html'), template.toString().replace('@@content', html));
+
+    }
 
 };
 
@@ -104,6 +114,6 @@ const buildIndex = async (dirPath) => {
     console.log('building content');
     await recurseForContent(archiveDirPath);
     console.log('building indexes');
-    await recurseForIndex(archiveDirPath);
+    await buildIndex(archiveDirPath);
 
 })();
